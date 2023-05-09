1c7db556b6f444a67f0ab76ec0fea373fc93774e
Hassen Ben Slama <hassen.slama@trivago.com>
2023-05-09 10:06:45 +0200

    use dotenv, better loggin

--- src/bin/serve.ts
+++ src/bin/serve.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
+
 import { App } from '../main/app.js';
 
+dotenv.config();
 const app = new App();
 
 try {
     await app.start();
 } catch (error: any) {
-    app.logger.error('Oops, something went wrong!');
+    app.logger.error('Oops, something went wrong!', error);
     process.exit(1);
 }
